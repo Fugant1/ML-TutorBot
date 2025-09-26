1848c95df801a6c5ff52c418179d25e8b0b3af7f
Fuganti <juliocesarfuganti@gmail.com>
2025-09-26 09:16:14 -0300

    test: scrapp tested, all good

--- src/app/rag_pipelines/scrapp_data.py
+++ src/app/rag_pipelines/scrapp_data.py
@@ -1,24 +1,21 @@
 from langchain_community.document_loaders.recursive_url_loader import RecursiveUrlLoader
 from bs4 import BeautifulSoup as Soup
+import requests
+import re
 
 def robust_html_extractor(html: str) -> str:
     soup = Soup(html, "lxml")
-    main_content = soup.find("main")
-    
-    if main_content:
-        return main_content.text
-    else:
-        return "" 
+    return re.sub(r"\n\n+", "\n\n", soup.text).strip()
 
 #Using user_guide to have all the important data of the site
 def scrapp_data(url):
     #using max_depth 2 as a test, we don't want to load ALL the site, just the main topics, most of the queries probably will not need to deep sklearn
-    loader = RecursiveUrlLoader(url=url, max_depth=2, extractor=robust_html_extractor)
+    loader = RecursiveUrlLoader(url=url, max_depth=1, extractor=robust_html_extractor)
 
     #after finishing the scrapps, I will test it :)
     return loader.load()
 
 #my test feature for now, lol ¬¬
-if __name__ == "__main__":
-     docs = scrapp_data('https://scikit-learn.org/stable/user_guide.htmls')
-     print(docs)
+# if __name__ == "__main__":
+#      docs = scrapp_data('https://scikit-learn.org/stable/user_guide')
+#      print(docs)
